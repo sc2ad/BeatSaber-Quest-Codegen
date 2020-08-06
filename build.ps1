dce264b773822e0efcff769678aaeede840e0f77
Adam ? <adamznow@gmail.com>
2020-08-06 15:53:29 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,4 +2,11 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Ignore"
 $PSDefaultParameterValues['*:ErrorAction']='Ignore'
 
-C:\android\sdk\ndk\21.3.6528147\ndk-build -j6 NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk V=0
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+$buildScript = "$NDKPath/build/ndk-build"
+if (-not ($PSVersionTable.PSEdition -eq "Core")) {
+    $buildScript += ".cmd"
+}
+
+& $buildScript -j6 NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
