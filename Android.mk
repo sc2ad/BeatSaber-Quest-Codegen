8a2208e7318d76177781c46138505604f24d53cc
Adam ? <adamznow@gmail.com>
2020-08-15 19:44:20 -0700

    Update bs-hook version, add gitignore, update publish script

--- Android.mk
+++ Android.mk
@@ -22,15 +22,16 @@ rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 # Writing prebuilt shared library: beatsaber-hook
 include $(CLEAR_VARS)
 LOCAL_MODULE := beatsaber-hook
-LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_0_4_2.so
+LOCAL_EXPORT_C_INCLUDES := ./extern/beatsaber-hook/shared/
+LOCAL_SRC_FILES := ./extern/libbeatsaber-hook_0_4_3.so
 LOCAL_CPP_FEATURES += rtti exceptions
 include $(PREBUILT_SHARED_LIBRARY)
+
 # Creating prebuilt for dependency: modloader - version: 0.1.1
 include $(CLEAR_VARS)
 LOCAL_MODULE := modloader
-LOCAL_EXPORT_C_INCLUDES := extern/modloader
-LOCAL_SRC_FILES := extern/libmodloader.so
+LOCAL_EXPORT_C_INCLUDES := ./extern/modloader/shared/
+LOCAL_SRC_FILES := ./extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 # Writing single library: il2cpp_codegen
@@ -40,6 +41,6 @@ LOCAL_SRC_FILES := $(call rwildcard,./src,*.cpp)
 LOCAL_SHARED_LIBRARIES += beatsaber-hook
 LOCAL_SHARED_LIBRARIES += modloader
 LOCAL_LDLIBS += -llog
-LOCAL_CFLAGS += -DMOD_ID='"il2cpp_codegen"' -DVERSION='"0.1.3"' -DNEED_UNSAFE_CSHARP -I'extern/libil2cpp/il2cpp/libil2cpp' -I'include' -Wno-inaccessible-base -DNO_CODEGEN_USE -isystem 'extern'
+LOCAL_CFLAGS += -DID='"il2cpp_codegen"' -DVERSION='"0.1.3"' -DNEED_UNSAFE_CSHARP -I'extern/libil2cpp/il2cpp/libil2cpp' -DNO_CODEGEN_USE -I'extern'
 LOCAL_C_INCLUDES += ./include
 include $(BUILD_SHARED_LIBRARY)
