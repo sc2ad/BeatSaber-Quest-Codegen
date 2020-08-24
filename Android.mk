a4a199c14c8aaba64a76c96fe6d47bae6117c1f3
Adam ? <adamznow@gmail.com>
2020-08-24 16:27:53 -0700

    For now remove this

--- Android.mk
+++ Android.mk
@@ -19,14 +19,6 @@ LOCAL_PATH := $(call my-dir)
 TARGET_ARCH_ABI := $(APP_ABI)
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
-# Writing prebuilt shared library: beatsaber-hook
-include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_0_4_4
-LOCAL_EXPORT_C_INCLUDES := ./extern/beatsaber-hook/shared/
-LOCAL_SRC_FILES := ./extern/libbeatsaber-hook_0_4_4.so
-LOCAL_CPP_FEATURES += rtti exceptions
-include $(PREBUILT_SHARED_LIBRARY)
-
 # Creating prebuilt for dependency: modloader - version: 0.1.1
 include $(CLEAR_VARS)
 LOCAL_MODULE := modloader
@@ -36,9 +28,8 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 # Writing single library: il2cpp_codegen
 include $(CLEAR_VARS)
-LOCAL_MODULE := il2cpp_codegen_0_1_4
+LOCAL_MODULE := il2cpp_codegen
 LOCAL_SRC_FILES := $(call rwildcard,./src,*.cpp)
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_0_4_4
 LOCAL_SHARED_LIBRARIES += modloader
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -DMOD_ID='"il2cpp_codegen"' -DVERSION='"0.1.4"' -Wall -Wextra -Werror -Wno-unused-function -DNEED_UNSAFE_CSHARP -DNO_CODEGEN_USE -isystem'extern/libil2cpp/il2cpp/libil2cpp' -isystem'extern'
