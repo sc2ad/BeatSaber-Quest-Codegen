e9ebfcc7c3444363d795db276a009367c1ffdaa5
Adam ? <adamznow@gmail.com>
2021-07-26 12:58:06 -0700

    Fix AlignmentUnion

--- include/System/Net/NetworkInformation/AlignmentUnion.hpp
+++ include/System/Net/NetworkInformation/AlignmentUnion.hpp
@@ -17,6 +17,20 @@ namespace System::Net::NetworkInformation {
   // [TokenAttribute] Offset: FFFFFFFF
   struct AlignmentUnion/*, public System::ValueType*/ {
     public:
+    struct __InternalUnionData {
+      // public System.Int32 Length
+      // Size: 0x4
+      // Offset: 0x0
+      int Length;
+      // Field size check
+      static_assert(sizeof(int) == 0x4);
+      // public System.Int32 IfIndex
+      // Size: 0x4
+      // Offset: 0x4
+      int IfIndex;
+      // Field size check
+      static_assert(sizeof(int) == 0x4);
+    };
     // Creating union for fields at offset: 0x0
     union {
       // public System.UInt64 Alignment
@@ -25,21 +39,12 @@ namespace System::Net::NetworkInformation {
       uint64_t Alignment;
       // Field size check
       static_assert(sizeof(uint64_t) == 0x8);
-      // public System.Int32 Length
-      // Size: 0x4
-      // Offset: 0x0
-      int Length;
-      // Field size check
-      static_assert(sizeof(int) == 0x4);
+      // WARNING: Manual union structure
+      __InternalUnionData data;
+      static_assert(sizeof(__InternalUnionData) == 0x8);
     };
-    // public System.Int32 IfIndex
-    // Size: 0x4
-    // Offset: 0x4
-    int IfIndex;
-    // Field size check
-    static_assert(sizeof(int) == 0x4);
     // Creating value type constructor for type: AlignmentUnion
-    constexpr AlignmentUnion(uint64_t Alignment_ = {}, int IfIndex_ = {}) noexcept : Alignment{Alignment_}, IfIndex{IfIndex_} {}
+    constexpr AlignmentUnion(uint64_t Alignment_ = {}) noexcept : Alignment{Alignment_} {}
     // Creating interface conversion operator: operator System::ValueType
     operator System::ValueType() noexcept {
       return *reinterpret_cast<System::ValueType*>(this);
