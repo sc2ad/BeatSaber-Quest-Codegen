976b226273f24de6b7d6d2c8aa7e2ea0df7aebe3
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-09-17 12:53:07 -0400

    Remove old stuff

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+#include "beatsaber-hook/shared/utils/base-wrapper-type.hpp"
+
+#include "UnityEngine/Color.hpp"
+
+// #include "UnityEngine/Object.hpp"
+
+// #include "GlobalNamespace/GlobalNamespace.hpp"
+
+
+extern "C" void setup() {
+  
+}
