30d7450ad9186c101d4a6dfba096c9b19dc1ba4b
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-09-21 20:00:43 +0200

    remove unused scripts and files

--- copy.ps1
+++
@@ -1,6 +0,0 @@
-& "$PSScriptRoot/build.ps1"
-
-adb shell am force-stop com.beatgames.beatsaber
-
-gci -rec -file -path libs -filter *.so  |
-    % { adb push $_.FullName /sdcard/Android/data/com.beatgames.beatsaber/files/libs/ }
